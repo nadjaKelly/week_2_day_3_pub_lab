ac98ce5e917b66cd4d4c144b79b732dd2268d72f
Nadja Kelly <nmfort-info@yahoo.de>
2018-12-02 23:39:44 +0000

    set up of all three classes

---
+++ customer.rb
@@ -0,0 +1,18 @@
+class Customer
+
+
+attr_reader :name, wallet
+
+def initialze(name, wallet)
+  @name = name
+  @wallet = wallet
+end
+
+
+
+
+
+
+
+
+end
---
+++ pub.rb
@@ -0,0 +1,24 @@
+class Pub
+
+
+attr_reader :name, :till, :drinks
+
+  def initialze(name, till, drinks)
+    @name = name
+    @till = till
+    @drinks = drinks
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
